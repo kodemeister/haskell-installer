56efe9860c73a045d0747a536a03d95f08b832c0
kodemeister <kodemeister@outlook.com>
2021-11-18 18:41:44 +0600

    Implement a general function to install GHC on Unix

---
+++ packages/common/org.haskell.ghc/meta/installscript.js
@@ -0,0 +1,31 @@
+function Component() {}
+
+installer.createGhcOperations = (ghcComponent, archive, archiveDir) => {
+  const version = ghcComponent.value("Version");
+
+  const tempDir = installer.generateTempPath("ghc");
+  const workingDir = archiveDir !== "" ? `${tempDir}/${archiveDir}` : tempDir;
+  const installDir = `@TargetDir@/ghc/${version}`;
+
+  const configureParams = [
+    "workingdirectory=" + workingDir,
+    "sh",
+    "./configure",
+    "--prefix=" + installDir,
+  ];
+
+  const makeParams = [
+    "workingdirectory=" + workingDir,
+    "make",
+    "install",
+    "UNDOEXECUTE",
+    "rm",
+    "-rf",
+    installDir,
+  ];
+
+  ghcComponent.addOperation("Extract", archive, tempDir);
+  ghcComponent.addOperation("Execute", configureParams);
+  ghcComponent.addOperation("Execute", makeParams);
+  ghcComponent.addOperation("Execute", "rm", "-rf", tempDir);
+};
