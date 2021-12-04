3efd66751887e3bddbff7cfd3916ac5fa6ab2967
kodemeister <kodemeister@outlook.com>
2021-12-04 06:59:01 +0600

    Simplify generation of temporary paths

--- packages/main/org.haskell.installer/meta/installscript.js
+++ packages/main/org.haskell.installer/meta/installscript.js
@@ -1,17 +1,16 @@
 class Component {
   constructor() {
+    // Export functions from this module via 'installer' global object.
     installer.generateTempPath = this.generateTempPath;
   }
 
-  generateTempPath(prefix = "tmp", length = 6) {
+  generateTempPath(prefix = "tmp", length = 16) {
     const tempDir = QDesktopServices.storageLocation(
       QDesktopServices.TempLocation
     );
-    const charSet =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const uuid = [...Array(length)]
-      .map(() => charSet[Math.floor(Math.random() * charSet.length)])
+      .map(() => Math.floor(Math.random() * 16).toString(16))
       .join("");
-    return `${tempDir}/${prefix}.${uuid}`;
+    return `${tempDir}/${prefix}-${uuid}`;
   }
 }
